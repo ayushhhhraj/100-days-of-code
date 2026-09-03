3bfe3bf0d0ef2d2dd789523d3985403cd22f073f
ayushhhhraj <ayushrajvns01@gmail.com>
2026-09-03 21:52:58 +0530

    Add files via upload

---
+++ day2q4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area, circumference;
+    const float PI = 3.14;
+
+    printf("Enter the radius of the circle:");
+    scanf("%f", &radius);
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+    printf("Area of the circle is %f\n", area);
+    printf("Circumference of the circle is %f\n", circumference);
+
+    return 0;
+}
